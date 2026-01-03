3bc57a8d96fc25a196df293c9c6e2fffd9d2a150
murugannagaraja781 <murugannagaraja781@gmail.com>
2026-01-03 18:05:31 +0530

    done

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,6 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+import axios from 'axios';
+
+if (import.meta.env.VITE_BACKEND_URL) {
+  axios.defaults.baseURL = import.meta.env.VITE_BACKEND_URL;
+}
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
